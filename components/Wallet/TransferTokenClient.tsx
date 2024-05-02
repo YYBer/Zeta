3ecfb36649aefdbfcd79f1ad5a043c43baa5de57
Pan <125814787+Pan-John@users.noreply.github.com>
2024-05-02 18:47:17 +0800

    transfer with storage deposit and get metadata

--- components/Wallet/TransferTokenClient.tsx
+++ components/Wallet/TransferTokenClient.tsx
@@ -4,14 +4,38 @@ import { useWalletSelector } from '@/components/contexts/WalletSelectorContext';
 import { useEffect, useState } from 'react';
 import { TransferAction } from '@near-wallet-selector/core';
 import * as nearAPI from "near-api-js";
-const { utils } = nearAPI;
+import { register } from 'module';
+import { deployContract } from 'near-api-js/lib/transaction';
+const { utils, keyStores, connect, Contract } = nearAPI;
+type ContractType = InstanceType<typeof Contract>;
+interface NEP141_Contract extends ContractType {
+  ft_balance_of: (args: { account_id: string }) => Promise<string>;
+  storage_balance_of: (args: { account_id: string }) => Promise<{total: string, available: string}>;
+  storage_balance_bounds: (args: { account_id: string }) => Promise<{min: string, max: string}>;
+  ft_metadata: () => Promise<{
+    spec: string;
+    name: string;
+    symbol: string;
+    icon: string;
+    reference: null | string;
+    reference_hash: null | string;
+    decimals: number;
+  }>;
+}
 
 const THIRTY_TGAS = '30000000000000';
+
+// testnet tokenlist
+// const TOKEN_LIST: { [key: string]: string } = {
+//   NEAR: 'NEAR',
+//   USDC: 'usdc.fakes.testnet'
+//   // Add other token symbols and their contract IDs
+// };
+
+// // mainnet tokenlist
 const TOKEN_LIST: { [key: string]: string } = {
-  NEAR: 'NEAR',
-  PTC: 'ft5.0xpj.testnet',
-  USDC: 'usdc.fakes.testnet'
-  // Add other token symbols and their contract IDs
+  // NEAR: 'NEAR', neeed to used wrapper contract
+  USDC: '17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1'
 };
 
 interface Payload {
@@ -58,48 +82,136 @@ export function TransferToken({ payload }: { payload: Payload }) {
       if (!accountId) {
         throw new Error('No active account found');
       }
-      const args = {
-        receiver_id: payload.receiverId,
-        amount: BigInt(Math.round(parseFloat(payload.amount) * 1e6)).toString(),
+
+      const user_id = payload.userId;
+      const receiver_id = payload.receiverId;
+      const symbol = payload.symbol;
+      const tokenContractId = TOKEN_LIST[symbol];
+
+      // // testnet config
+      // const connectionConfig = {
+      //   networkId: "testnet",
+      //   keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+      //   nodeUrl: "https://rpc.testnet.near.org",
+      //   walletUrl: "https://testnet.mynearwallet.com/",
+      //   helperUrl: "https://helper.testnet.near.org",
+      //   explorerUrl: "https://testnet.nearblocks.io",
+      // };
+
+      // // mainnet config
+      const connectionConfig = {
+        networkId: "mainnet",
+        keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+        nodeUrl: "https://rpc.mainnet.near.org",
+        walletUrl: "https://wallet.mainnet.near.org",
+        helperUrl: "https://helper.mainnet.near.org",
+        explorerUrl: "https://nearblocks.io",
       };
-      const contractId = TOKEN_LIST[payload.symbol];
-      console.log("Contract ID: ", contractId);
-      console.log("The args sent: ", args);
- 
-      const transferAction: TransferAction = {
+
+      const nearConnection = await connect(connectionConfig);
+      const receiver_account = await nearConnection.account(user_id);
+      
+      const contract = new Contract(receiver_account, tokenContractId, {
+          viewMethods: ['storage_balance_of', 'storage_balance_bounds', 'ft_metadata'],
+          changeMethods: [],
+      }) as unknown as NEP141_Contract;
+
+      // get decimals of token contract
+      const metadata = await contract.ft_metadata();
+      const decimals = metadata.decimals;
+      const unit_convert = 10**decimals
+      console.log(`Decimals of ${symbol} contract: ${unit_convert}`);
+    
+      // get storage balance of receiver first
+      const storage_balance_of_receiver = await contract.storage_balance_of({ account_id: receiver_id });
+      console.log(`Storage Balance of ${receiver_id} in ${symbol} contract: ${storage_balance_of_receiver}`);        
+
+      // get storage balance bounds
+      const bounds = await contract.storage_balance_bounds({ account_id: receiver_id });
+      const amount_should_be_deposit = bounds.min;
+
+      // // sender way
+      // const stroage_deposit ={
+      //   receiverId: tokenContractId,
+      //   actions:[
+      //     {
+      //       methodName: 'storage_deposit',
+      //       args: {
+      //         accountId: receiver_id,
+      //         registeration_only: true
+      //       },
+      //       gas: THIRTY_TGAS,
+      //       deposit: amount_should_be_deposit
+      //     }
+      //   ]
+      // }
+
+      // [action] storage deposit for receiver
+      const args_storageDeposit = {
+          account_id: receiver_id,
+      }
+      const storageDepositForReceiver: FunctionCallAction = {
+          type: 'FunctionCall',
+          params: {
+              methodName: 'storage_deposit',
+              args: args_storageDeposit,
+              gas: THIRTY_TGAS,
+              deposit: amount_should_be_deposit
+          }
+      }
+
+      // [action] transfer NEAR
+      const transferNear: TransferAction = {
         type: 'Transfer',
         params: {
           // @ts-ignore
           deposit: utils.format.parseNearAmount(payload.amount.toString())
         }
       };
-
-      const transfer: FunctionCallAction = {
+      
+      // [action] transfer token
+      const args_transferToken = {
+        receiver_id: payload.receiverId,
+        amount: BigInt(Math.round(parseFloat(payload.amount) * unit_convert)).toString(),
+      };
+      const transferToken: FunctionCallAction = {
         type: 'FunctionCall',
         params: {
           methodName: 'ft_transfer',
-          args: args,
+          args: args_transferToken,
           gas: THIRTY_TGAS,
           deposit: '1'
         }
+      }      
+
+      if (payload.symbol == 'NEAR') {
+        await wallet.signAndSendTransaction({
+          receiverId: payload.receiverId,
+          actions: [transferNear],
+        });
+      } 
+      else if (!storage_balance_of_receiver) {
+        console.log("reciever not registered!")
+
+        await wallet.signAndSendTransactions({
+          transactions:[
+            {
+              receiverId: tokenContractId,
+              actions: [storageDepositForReceiver, transferToken]
+            },
+            // {
+            //   receiverId: tokenContractId,
+            //   actions: [transferToken]
+            // }
+          ]
+        })
+      } 
+      else {
+        await wallet.signAndSendTransaction({
+          receiverId: tokenContractId,
+          actions: [transferToken]
+        })
       }
-    
- if (payload.symbol == 'NEAR')
- {
-      await wallet.signAndSendTransaction({
-        receiverId: payload.receiverId,
-        actions: [transferAction],
-      });
-
-    }
-else 
-   { 
-    await wallet.signAndSendTransaction({
-      receiverId: contractId,
-      actions: [transfer]
-    })
-
-  }
 
       setSuccess(true);
       console.log(
